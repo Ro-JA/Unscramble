ac8820b8705b9c5c803f12752159551cd4939201
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-01-21 21:01:58 +0900

    correct

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,44 +6,53 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    private var score = 0
-    private var currentWordCount = 0 // количество слов в игре
-    private var _currentScrambledWord = "test" // зашифрованое слово
+
+
+    private var _score = 0
+    val score: Int
+        get() = _score
+    private var _currentWordCount = 0 // количество слов в игре
+    val currentWordCount: Int
+        get() = _currentWordCount
+    private lateinit var _currentScrambledWord: String// зашифрованое слово
     val currentScrambledWord: String
         get() = _currentScrambledWord // гетар для фрагмента для безопасности
 
 
-    private val wordList: MutableList<String> = mutableListOf() // изменяемый список слов который мы
+    private val wordsList: MutableList<String> = mutableListOf() // изменяемый список слов который мы
     // храним в игре чтобы избежать повторений
     private lateinit var currentWord: String // слова которое пытаеться рашифровать игорок
 
-
-
-
-    private fun getNextWord() {
-        currentWord = allWordsList.random() // получает случайное слово из списка слов
-
-        val tempWord = currentWord.toCharArray() // получаем массив сисмволов из случайного слова
-        while (String(tempWord).equals(currentWord, false)) { // пока массив символов
-            //равен случайному слову тусовать символы
-            tempWord.shuffle() // в этом массиве символо тусуем значения
-        }
-        if (wordList.contains(currentWord)) {
-            getNextWord() // если список уже содержит случай слово, создаем новое слова
-        } else {
-            _currentScrambledWord = String(tempWord) // добовляем его в зашифрованое слово
-            ++currentWordCount // увеличевае счетчик слов
-            wordList.add(currentWord) // добовляем его в список слов чтобы больше не повторялось
-
-        }
-    }
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord() // вызываем случайно слово
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
+
+
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+
+
 
 }
