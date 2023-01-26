bc247f37d65634f8690838f4ce4177fc5b310f4c
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-01-26 23:06:19 +0900

    add liveData string

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -72,15 +72,6 @@ class GameFragment : Fragment() {
         // Setup a click listener for the Submit and Skip buttons.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
-        viewModel.score.observe(viewLifecycleOwner, { newScore ->
-            binding.score.text = getString(R.string.score, newScore)// добавили очки из лайвДата
-        })
-
-        viewModel.currentWordCount.observe(viewLifecycleOwner,
-            { newWordCount ->
-                binding.wordCount.text =
-                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
-            })
     }
 
     /*
